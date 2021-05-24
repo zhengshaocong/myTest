c69da3a00a689e718a8a8eab931f5f2e3b7da79a
zhengshaocong <zhengshaocong@beejeen.com>
2021-05-24 17:42:50 +0800

    源码

---
+++ src/index.js
@@ -0,0 +1,30 @@
+
+import h from './mysnabbdom/h'
+
+import patch from './mysnabbdom/patch'
+
+const container=document.getElementById('container')
+
+// const myVnode1=h('h1',{},'你好')
+let myVnode1=h('p',{},[
+    h('div',{key:'A'},'A'),
+    h('div',{key:'E'},'E'),
+    h('div',{key:'B'},'B'),
+    h('div',{key:'C'},'C'),
+    h('div',{key:'D'},'D'),
+])
+let myVnode2=h('p',{},[
+    // h('div',{key:'A'},'A'),
+    // h('div',{key:'Z'},'Z'),
+    // h('div',{key:'E'},'E'),
+    // h('div',{key:'K'},'K'),
+    // h('div',{key:'B'},'B'),
+    h('div',{key:'G'},'G'),
+])
+patch(container,myVnode1)
+setTimeout(()=>{
+    patch(myVnode1,myVnode2)
+},500)
+
+
+
